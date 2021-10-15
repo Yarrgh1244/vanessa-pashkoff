de42f203e6ac1d0854bb43eb437b5e6a7a8c765a
Yarrgh1244 <smithale@gmail.com>
2021-10-15 10:55:33 -0400

    updated list of topics in Workshops

---
+++ src/pages/Workshops.vue
@@ -0,0 +1,30 @@
+<template>
+  <Layout>
+    <div class="bg-splash-3 bg-contain flex flex-col flex-grow overflow-hidden pt-12 ">
+      <div class="container-inner mx-auto py-16 bg-white">
+        <h2 class="text-4xl font-bold mb-16">Workshops</h2>
+        <h1 class="text-xl font-bold">Vanessa is available for virtual AND in-person workshops on topics including:
+           </h1>
+          <ul class="mt-8">
+            <li>Family Engagement and Partnership</li>
+            <li>Emergent Literacy in Early Childhood Classrooms</li>
+            <li>AntI-Bias Education</li>
+            <li>Storytelling and Story Acting with Young Children</li>
+            <li>Loose Parts and Open Ended Provocations</li>
+            <li>STEAM with Infants and Toddlers</li>
+            <li>Play and Risk Taking</li>
+            <li>Classroom Environments</li>
+          </ul>           
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Workshops'
+  }
+}
+</script>
+
